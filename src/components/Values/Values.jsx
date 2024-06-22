d380dc5670b2878e363d452d151066a8ce887170
VarshaPandey24 <pandeyv6639@gmail.com>
2024-06-22 22:25:22 +0530

    value section created

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import "./Values.css";
 import {
   Accordion,
   AccordionItem,
@@ -8,10 +10,6 @@ import {
   AccordionItemPanel,
   AccordionItemState,
 } from "react-accessible-accordion";
-
-import "react-accessible-accordion/dist/fancy-example.css";
-import { MdOutlineArrowDropDown } from "react-icons/md";
-import "./Values.css";
 import "../../utils/accordion";
 
 const Values = () => {
@@ -31,32 +29,13 @@ const Values = () => {
             We always ready to help by providijng the best services for you.
             <br /> We beleive a good blace to live can make your life better
           </span>
+
           <Accordion
             className="accordion"
             allowMultipleExpanded={false}
             preExpanded={[0]}
-          >{
-            data.map((item,i) => {
-                    return(
-                            <AccordionItem class="accoritem" key ={i} vvid={i}>
-         <AccordionItemHeading>
-            <AccordionItemButton>
-                <div className="flexcenter icon">
-                    {item.icon}
-                </div>
-                <span className="primaryText">
-                    {item.heading}
-                </span>
-                <div className="flexcenter icon">
-                    <MdOutlineArrowDropDown size ={20}/>
-                    </div>
-            </AccordionItemButton>
-         </AccordionItemHeading>
-                            </AccordionItem>
-                    )
-                })
-              }    
-            
+          >
+            {}
           </Accordion>
         </div>
       </div>
